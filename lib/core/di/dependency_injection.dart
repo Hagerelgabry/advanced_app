217684a12e433ff5879900ea6d2d9cf3c0d71dda
Hagerelgabry <hagerfcis@gmail.com>
2024-11-05 12:34:53 +0200

    - signup screen UI and Api

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -2,6 +2,8 @@ import 'package:advanced_app/core/networking/api_service.dart';
 import 'package:advanced_app/core/networking/dio_factory.dart';
 import 'package:advanced_app/features/login/data/repos/login_repo.dart';
 import 'package:advanced_app/features/login/logic/cubit/login_cubit.dart';
+import 'package:advanced_app/features/signup/data/repos/signup_repo.dart';
+import 'package:advanced_app/features/signup/logic/cubit/signup_cubit.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -15,5 +17,9 @@ Future<void> setupGetIt() async {
 
   //login
   getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
-  getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
+
+  //signup
+  getIt.registerLazySingleton<SignupRepo>(() => SignupRepo(getIt()));
+  getIt.registerFactory<SignupCubit>(() => SignupCubit(getIt()));
 }
