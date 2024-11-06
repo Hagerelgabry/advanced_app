c42a0af2cfc1d7b6206b506db758aaf8f4e13383
Hagerelgabry <hagerfcis@gmail.com>
2024-11-06 11:00:54 +0200

    new main files

---
+++ lib/main_production.dart
@@ -0,0 +1,11 @@
+import 'package:advanced_app/core/di/dependency_injection.dart';
+import 'package:advanced_app/core/routing/app_router.dart';
+import 'package:advanced_app/doc_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  setupGetIt();
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}
