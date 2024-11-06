b2dc2bb72f22d510ba90461dbb794e1c9f080692
Hagerelgabry <hagerfcis@gmail.com>
2024-11-06 11:59:14 +0200

    change spacing

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -51,7 +51,7 @@ class LoginScreen extends StatelessWidget {
                         validateThenDoLogin(context);
                       },
                     ),
-                    verticalSpace(16),
+                    verticalSpace(20),
                     const TermsAndConditionsText(),
                     verticalSpace(60),
                     const DontHaveAccountText(),
